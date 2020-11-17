df2744b0fa808c6a9912a64530b94dee78984daa
wangpp <987897856@qq.com>
2020-11-17 18:34:58 +0800

    leetcode239

---
+++ src/leetcode239/win.java
@@ -0,0 +1,25 @@
+package leetcode239;
+
+public class win {
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;//窗口大小
+        int len = nums.length;
+        int[] win = new int[len-k+1];
+        //遍历所有的滑动窗口
+        for(int i=0;i<win.length;i++){
+            int max = Integer.MIN_VALUE;
+            //找到每个窗口的最大值
+            for(int j=i;j<i+k;j++){
+                max = Math.max(max,nums[j]);
+            }
+            win[i] = max;
+        }
+
+        for(int i=0;i<win.length;i++){
+            System.out.print(win[i]);
+        }
+
+    }
+}
---
+++ src/leetcode239/winPro.java
@@ -0,0 +1,44 @@
+package leetcode239;
+
+import java.util.LinkedList;
+
+/**
+ * 双端队列
+ */
+public class winPro {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int k = 3;//窗口大小
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        int[] win = new int[nums.length-k+1];
+
+        for(int i=0;i<nums.length;i++){
+            //保证从大到小，如果前面数小，弹出
+            while (!linkedList.isEmpty() && nums[linkedList.peekLast()] <= nums[i]){
+                linkedList.pollLast();
+            }
+
+            //添加当前值对应的数组下标
+            linkedList.addLast(i);
+            //初始化窗口，等到窗口长度为k时，下次移动删除过期数据
+            if(linkedList.peek()<=i-k){
+                linkedList.poll();
+
+            }
+            if(i-k+1>=0){
+                win[i-k+1]=nums[linkedList.peek()];
+            }
+
+        }
+
+        for(int i=0;i<win.length;i++){
+            System.out.println(win[i]);
+        }
+
+    }
+
+}
